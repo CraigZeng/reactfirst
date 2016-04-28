2a501b6db7be6093fd891fae53ac852cd5ad1de7
Zeng Cheng <zengcheng@zufangit.cn>
2016-04-28 16:12:59 +0800

    fixed validate

--- app/views/common/Layout.jsx
+++ app/views/common/Layout.jsx
@@ -1,22 +1,20 @@
-import React, { Component, PropTypes } from 'react';
+import React, { PropTypes } from 'react';
 import { Link } from 'react-router';
 
-class Layout extends Component {
-  render(){
-    return (
-        <div>
-          <div className="nav">
-            <ul>
-              <li><Link to="/home">Home</Link></li>
-              <li><Link to="/about">About</Link></li>
-            </ul>
-          </div>
-          <div className="content">
-            {this.props.children}
-          </div>
-        </div>
-    );
-  }
+function Layout({ children }) {
+  return (
+    <div>
+      <div className="nav">
+        <ul>
+          <li><Link to="/home">Home</Link></li>
+          <li><Link to="/about">About</Link></li>
+        </ul>
+      </div>
+      <div className="content">
+        {children}
+      </div>
+    </div>
+  );
 }
 
 Layout.propTypes = {
